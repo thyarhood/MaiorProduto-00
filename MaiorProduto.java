2b1cb7a456ddb0c40ad80cab8ad527260ceec67f
Breno Henrique <thyarhood@gmail.com>
2022-06-16 17:44:56 -0300

    add: adicionei primeira versao do codigo

---
+++ MaiorProduto.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class MaiorProduto {
+	public static void main(String args[]) {
+	
+		Scanner sc = new Scanner(System.in);
+		String numeros;
+		System.out.println("Digite a sequencia de numeros: ");
+		numeros = sc.nextLine();
+		
+		int long_String;
+		long_String = numeros.length();
+		
+		//Colocar cada letra em um vetor
+		String letras_vetor[];
+		letras_vetor = new String[long_String];
+		int a = 0, b = 1; 
+		for(int i = 0; i < long_String; i++) {
+		letras_vetor[i] = numeros.substring(a,b);	
+		a++;
+		b++;
+		//Status == funcionando//
+		}
+		
+		//Transforma cada letra em um numeros
+		int numeros_vetor[];
+		numeros_vetor = new int[long_String];
+		for(int i = 0; i < long_String; i++) {
+		numeros_vetor[i] =  Integer.valueOf(letras_vetor[i]); 
+		//Status == funcioando//
+		}
+			
+			//Faz a multiplicaçao entre os termos
+			int resultado[];
+			resultado = new int[long_String];
+			resultado[0] = 1;
+		
+					int j = 0;
+		
+						for(int i = 0; i < long_String; i++) {
+						j++;
+						if(i <= 1){
+						j = 0;	
+						}
+						resultado[i] = numeros_vetor[i] * resultado[j];
+						System.out.println(resultado[i]);
+						}
+			//Status == funcionando
+		
+		
+	}
+}
